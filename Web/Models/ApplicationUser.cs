b1d15bf06909a18e465e6e9aa5d59659126e489b
magnuscarbonnier <magnus.carbonnier@gmail.com>
2020-04-27 14:30:52 +0200

    add dataannotations add item class

--- Web/Models/ApplicationUser.cs
+++ Web/Models/ApplicationUser.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,11 +9,22 @@ namespace Web.Models
 {
     public class ApplicationUser:IdentityUser
     {
+        [Display(Name = "Förnamn")]
         public string FirstName { get; set; }
+
+        [Display(Name = "Efternamn")]
         public string LastName { get; set; }
+
+        [Display(Name = "Adress")]
         public string Address { get; set; }
+
+        [Display(Name = "Postnummer")]
         public string ZipCode { get; set; }
+
+        [Display(Name = "Stad")]
         public string City { get; set; }
+
+        [Display(Name = "Land")]
         public string Country { get; set; }
     }
 }
