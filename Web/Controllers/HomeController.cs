8b7f4a1f3c921f25a2333c57715a74a533161edd
magnuscarbonnier <magnus.carbonnier@gmail.com>
2020-05-02 21:09:48 +0200

    fdsa

--- Web/Controllers/HomeController.cs
+++ Web/Controllers/HomeController.cs
@@ -6,21 +6,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Web.Models;
+using Web.Services;
+using Web.ViewModels;
 
 namespace Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IProductService _productService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IProductService productService)
         {
             _logger = logger;
+            _productService = productService;
         }
 
         public IActionResult Index()
         {
-            return View();
+            HomeViewModel vm = new HomeViewModel();
+            var products = _productService.GetAll().OrderByDescending(c=>c.Price).Take(3);
+            vm.Top3Products = products;
+            return View(vm);
         }
 
         public IActionResult Privacy()
