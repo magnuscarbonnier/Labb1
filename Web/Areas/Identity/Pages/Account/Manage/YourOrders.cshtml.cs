7126f2e5347dd43957063b8aaa8031df0fe0c9bf
magnuscarbonnier <magnus.carbonnier@gmail.com>
2020-05-02 19:52:33 +0200

    productdesign add partial

--- Web/Areas/Identity/Pages/Account/Manage/YourOrders.cshtml.cs
+++ Web/Areas/Identity/Pages/Account/Manage/YourOrders.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Web.Models;
@@ -12,9 +13,20 @@ namespace Web.Areas.Identity.Pages.Account.Manage
 {
     public class YourOrders : PageModel
     {
+        private readonly IProductService _productService;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public YourOrders(IProductService productService, UserManager<ApplicationUser> userManager)
+        {
+            _productService = productService;
+            _userManager = userManager;
+        }
+        
+            
         public List<Order> Orders = new List<Order>();
         public void OnGet()
         {
+            HttpContext.Session.CheckUserId(HttpContext, _userManager);
             var orderresponse = HttpContext.Session.Get<List<Order>>(Lib.SessionKeyOrderList);
         if(orderresponse!=null)
             {
